5fa9334dcc948e020e33ba7ace770367e396c337
gopalwayindia <146926280+gopalwayindia@users.noreply.github.com>
2023-10-24 20:16:18 +0530

    story book api

---
+++ app.js
@@ -0,0 +1,135 @@
+//-----------------Add Story----------------------
+
+async function addStory() {
+  
+    let story = {
+      title: document.getElementById("title").value,
+      type: document.getElementById("type").value,
+      image: document.getElementById("image").value,
+      content: document.getElementById("content").value,
+    };
+  
+    let response = await fetch("http://localhost:3001/story", {
+      method: "POST",
+      body: JSON.stringify(story), // Pass the story object here
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      // Handle success
+      alert("Story added successfully");
+      console.log("Story added successfully");
+    } else {
+      // Handle errors
+      console.error("Failed to add the story");
+    }
+  }
+  
+
+  //-----------------Get All Stories----------------------
+
+  let localUrl = "http://127.0.0.1:3001/story";
+  let getData = async () => {
+    let res = await fetch(localUrl);
+    let data = await res.json();
+    append(data);
+    return data.items;
+  };
+
+  getData();
+
+  let append = (data) => {
+    console.log(data);
+  
+    let container = document.querySelector("tbody");
+    container.innerHTML = null;
+  
+    data.forEach((el) => {
+      let pID = document.createElement("td");
+      pID.innerText = el.id;
+      let title = document.createElement("td");
+      title.innerText = el.title;
+      let type = document.createElement("td");
+      type.innerText = el.type;
+      let content = document.createElement("td");
+      content.innerText = el.content;
+      let image = document.createElement("td");
+      image.innerText = el.image;
+      
+      // Action buttons
+      let updateButton = document.createElement("button");
+      updateButton.innerText = "Update";
+      updateButton.addEventListener("click", () => openUpdateModal(el));
+      
+      let deleteButton = document.createElement("button");
+      deleteButton.innerText = "Delete";
+      deleteButton.addEventListener("click", () => openDeleteModal(el));
+      
+      let viewButton = document.createElement("button");
+      viewButton.innerText = "View";
+      viewButton.addEventListener("click", () => openViewModal(el));
+  
+      // Create a cell for the action buttons
+      let actions = document.createElement("td");
+      actions.appendChild(updateButton);
+      actions.appendChild(deleteButton);
+      actions.appendChild(viewButton);
+  
+      let tr = document.createElement("tr");
+      tr.append(pID, title, type, content, image, actions);
+      container.append(tr);
+    });
+  };
+  
+
+// Function to open the update modal with story details
+function openUpdateModal(story) {
+    // Populate the modal fields with story details
+    console.log(story.id);
+    document.getElementById("utitle").value = story.title;
+    document.getElementById("utype").value = story.type;
+    document.getElementById("ucontent").value = story.content;
+    document.getElementById("uimage").value = story.image;
+    
+    // Attach the story ID to the "Update" button
+    document.getElementById("updateStory").dataset.storyId = story.id;
+
+    $("#updateModal").modal("show");
+}
+
+// Function to update a story
+async function updateStory() {
+    const storyId = document.getElementById("updateStory").dataset.storyId;
+
+    let updatedStory = {
+        title: document.getElementById("utitle").value,
+        type: document.getElementById("utype").value,
+        content: document.getElementById("ucontent").value,
+        image: document.getElementById("uimage").value,
+    };
+
+    try {
+        const response = await fetch(`http://127.0.0.1:3001/story/${storyId}`, {
+            method: "PUT",
+            body: JSON.stringify(updatedStory),
+            headers: {
+                "Content-type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            // Handle success
+            alert("Story updated successfully");
+            console.log("Story updated successfully");
+            // Close the update modal if needed
+            $("#updateModal").modal("hide");
+        } else {
+            // Handle errors
+            console.error("Failed to update the story");
+        }
+    } catch (error) {
+        console.error("An error occurred while updating the story:", error);
+    }
+}
