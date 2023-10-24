9cadc5b3ca3462b562e7d9c4ddb8a3abf25bd491
gopalwayindia <146926280+gopalwayindia@users.noreply.github.com>
2023-10-24 21:33:58 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const PORT = process.env.PORT || 3001;
+// const path = require('path');
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.use(router);
+// server.listen(PORT,()=> console.log(`JSON Server is running on port${PORT}`));
+
+
+
+const PORT = 3000;
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname,'db.json'));
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 3001;
-const path = require('path');
-
 server.use(middlewares);
-server.use(router);
-
-server.use(router);
+server.use(jsonServer.bodyParser);
+server.use('/api',router);
 server.listen(PORT,()=> console.log(`JSON Server is running on port${PORT}`));
